1e1dc7854c9e80b1a87e68164bd48f8dfa3fb415
Emma Dickson <ejdickson93@gmail.com>
2019-02-15 10:35:51 -0500

    starter pseudocode for js

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// if statement for selecting easy/medium/hard button
+    // if easy function(1 second)
+    // if medium function(1/2 second)
+    // if hard function(1/4 second)
+
+// one function containing game play logic taking in time as a variable
+    // empty array chain of events
+    // add to chain of events array by random number generator (0-3), each number represents a color (function?)
+    // if player array matches event chain array
+        // add 1 to score
+        // add another event to chain
+    // if player array does not match event chain array
+        // alert game over
+
+
+// game over function
+    // if player score > lowest leaderboard score [leaderboard array/objects]
+        // add player score to leader board in position [maybe switch statement?]
