732096ecd6c940a9d16b0bb562c79f39b1a58a50
Emma Dickson <ejdickson93@gmail.com>
2019-02-18 18:32:32 -0500

    made code slightly DRY for lighting/sounding buttons, could probably be better

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,48 @@
 // one sheet of all the functions that will be called during game play
 
+// function: one for each of the light & tone buttons
+
+let lightGreen = function(time) {
+    $('.greenButton').toggleClass('bright'); 
+    $('#lightsaber')[0].play();
+    setTimeout(function() { 
+        $('.greenButton').toggleClass('bright');
+        $('#lightsaber')[0].pause();
+        $('#lightsaber')[0].currentTime = 0;
+    }, time);
+}
+
+let lightRed = function(time) {
+    $('.redButton').toggleClass('bright');
+    $('#lasers')[0].play();
+    setTimeout(function() { 
+        $('.redButton').toggleClass('bright');
+        $('#lasers')[0].pause();
+        $('#lasers')[0].currentTime = 0;
+    }, time);
+}
+
+let lightYellow = function(time) {
+    $('.yellowButton').toggleClass('bright');
+    $('#chewie')[0].play();
+    setTimeout(function() { 
+        $('.yellowButton').toggleClass('bright');
+        $('#chewie')[0].pause();
+        $('#chewie')[0].currentTime = 0;
+    }, time);
+}
+
+let lightBlue = function(time) {
+    $('.blueButton').toggleClass('bright');
+    $('#r2d2')[0].play();
+    setTimeout(function() { 
+        $('.blueButton').toggleClass('bright');
+        $('#r2d2')[0].pause();
+        $('#r2d2')[0].currentTime = 0;
+    }, time);
+}
+
+
  // function: add to chain of events array by random number generator (1-4), each number represents a color
 let addSimon = function(arr1) {
     let nextLight = ((Math.floor(Math.random() * 4) + 1));
@@ -8,44 +51,25 @@ let addSimon = function(arr1) {
 }
 
 // function: Simon actions (light & tone i for time)
+// const lightSimon = async (arr1, time) => {
+//     await asyncForEach(arr1, async ())
+
+//     console.log(arr1)
+// }
 let lightSimon = function(arr1, time) {
     for (let i = 0; i < arr1.length; i++) { 
         switch (arr1[i]) {
             case 1:
-                $('.greenButton').toggleClass('bright'); 
-                $('#lightsaber')[0].play();
-                setTimeout(function() { 
-                    $('.greenButton').toggleClass('bright');
-                    $('#lightsaber')[0].pause();
-                    $('#lightsaber')[0].currentTime = 0;
-                }, time);
+                lightGreen(time);
                 break;
             case 2:
-                $('.redButton').toggleClass('bright');
-                $('#lasers')[0].play();
-                setTimeout(function() { 
-                    $('.redButton').toggleClass('bright');
-                    $('#lasers')[0].pause();
-                    $('#lasers')[0].currentTime = 0;
-                }, time);
+                lightRed(time);
                 break;
             case 3:
-                $('.yellowButton').toggleClass('bright');
-                $('#chewie')[0].play();
-                setTimeout(function() { 
-                    $('.yellowButton').toggleClass('bright');
-                    $('#chewie')[0].pause();
-                    $('#chewie')[0].currentTime = 0;
-                }, time);
+                lightYellow(time);
                 break;
             case 4:
-                $('.blueButton').toggleClass('bright');
-                $('#r2d2')[0].play();
-                setTimeout(function() { 
-                    $('.blueButton').toggleClass('bright');
-                    $('#r2d2')[0].pause();
-                    $('#r2d2')[0].currentTime = 0;
-                }, time);
+                lightBlue(time);
                 break;
         }
     }
@@ -56,46 +80,22 @@ let lightSimon = function(arr1, time) {
 // function: take player actions and input to an array that can be compared to chain of events
 
 let playerAction = function(arr2, time) {
-        $('.greenButton').click(function() {
-                $('.greenButton').toggleClass('bright'); 
-                $('#lightsaber')[0].play();
-                setTimeout(function() { 
-                    $('.greenButton').toggleClass('bright'); 
-                    $('#lightsaber')[0].pause();
-                    $('#lightsaber')[0].currentTime = 0;
-                }, time);
-                arr2.push(1);
-        });
-        $('.redButton').click(function() {
-                $('.redButton').toggleClass('bright');
-                $('#lasers')[0].play();
-                setTimeout(function() { 
-                    $('.redButton').toggleClass('bright');
-                    $('#lasers')[0].pause();
-                    $('#lasers')[0].currentTime = 0;
-                }, time);
-                arr2.push(2);
-        });
-        $('.yellowButton').click(function() {
-                $('.yellowButton').toggleClass('bright');
-                $('#chewie')[0].play();
-                setTimeout(function() { 
-                    $('.yellowButton').toggleClass('bright');
-                    $('#chewie')[0].pause();
-                    $('#chewie')[0].currentTime = 0;
-                }, time);
-                arr2.push(3);
-        });
-        $('.blueButton').click(function() {
-                $('.blueButton').toggleClass('bright');
-                $('#r2d2')[0].play();
-                setTimeout(function() { 
-                    $('.blueButton').toggleClass('bright');
-                    $('#r2d2')[0].pause();
-                    $('#r2d2')[0].currentTime = 0;
-                }, time);
-                arr2.push(4);
-        });
+    $('.greenButton').click(function() {
+        lightGreen(time);
+        arr2.push(1);
+    });
+    $('.redButton').click(function() {
+        lightRed(time);
+        arr2.push(2);
+    });
+    $('.yellowButton').click(function() {
+        lightYellow(time);
+        arr2.push(3);
+    });
+    $('.blueButton').click(function() {
+        lightBlue(time);
+        arr2.push(4);
+    });
 }
 
 
@@ -103,7 +103,7 @@ let playerAction = function(arr2, time) {
 let checkArray = function(arr1, arr2) {
     i = 0;
     while (i < arr1.length + 1) {
-        // if playerArray === chainEvents, keep playing
+        // if playerArray === chainEvents, keep going
         if ((arr2[i]) === (arr1[i])) {
             i++
         } else {
--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
     // if easy function(time = 1000)
     // if medium function(time = 500)
     // if hard function(time = 750)
+    // once choose one, toggle hidden
 
 // on any page, hover buttons to see color change
 // $('.gameboard div').hover(function() {
@@ -11,36 +12,56 @@
 // one function containing game play logic taking in time as a variable
 
 // count down 3...2...1 go!
-    
-var chainEvents = [];
-var playerArray = [];
-var playerScore = 0;
 
-// first move
+async function playGame() {
+    // reset Simon Chain, Player Chain, and Player Score when starting a new game
+    var chainEvents = [];
+    var playerArray = [];
+    var playerScore = 0;
 
-addSimon(chainEvents);
-lightSimon(chainEvents, 1000);
+    // first move
 
-playerAction(playerArray, 1000);
+    addSimon(chainEvents);
+    lightSimon(chainEvents, 1000);
 
-if (playerArray === chainEvents) {
-    while (checkArray(chainEvents, playerArray) === true) {
-        addSimon(chainEvents);
-        lightSimon(chainEvents, 1000);
-        playerAction(playerArray, 1000);
+    playerAction(playerArray, 1000);
 
-        playerScore = Number(playerScore) + 1;
-        $('#score h3').text(playerScore);
-        
-    }
-} else {
-    console.log('GAME OVER!');
+    // check check player moves
+
+    // await checkArray(chainEvents, playerArray)
+
+    // if (playerArray === chainEvents) {
+    //     while (checkArray(chainEvents, playerArray) === true) {
+            
+    //         // update score
+    //         playerScore = Number(playerScore) + 1;
+    //         $('#score h3').text(playerScore);
+
+    //         //start new round
+    //         addSimon(chainEvents);
+    //         lightSimon(chainEvents, 1000);
+    //         playerAction(playerArray, 1000);
+            
+    //     }
+    // } else {
+    //     console.log('GAME OVER!');
+    // }
+
+
 }
+
+playGame()
+
+
+
+
+
+
 
 
 // while player is still matching the chainEvents, add to chainEvents
 
-    
+
 
 
 
